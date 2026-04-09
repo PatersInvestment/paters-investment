777ab564e907871fac6a172de12305cd4e6be0d5
Siyabonga Mabuza <siyakevin316@gmail.com>
2026-04-09 21:08:36 +0200

    fix: correct base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
   plugins: [react()],
-  base: '/PatersInvestment/', // This must match your repo name exactly
+  base: '/', // 👈 THIS is the fix
 })
